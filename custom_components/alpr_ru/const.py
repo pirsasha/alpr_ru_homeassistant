9c7a9ec6c65663a2fdfeb369d275f14d27934cc0
Aleksandr <166982493+pirsasha@users.noreply.github.com>
2026-09-06 15:47:10 +0300

    feat: add access control settings constants

--- custom_components/alpr_ru/const.py
+++ custom_components/alpr_ru/const.py
@@ -10,12 +10,23 @@ CONF_CAMERA_ENTITY = "camera_entity"
 CONF_TRIGGER_ENTITY = "trigger_entity"
 CONF_PLATE_TYPE = "plate_type"
 
+CONF_ACCESS_ENABLED = "access_enabled"
+CONF_ALLOWED_PLATES = "allowed_plates"
+CONF_GATE_ENTITY = "gate_entity"
+CONF_MIN_CONFIDENCE = "min_confidence"
+CONF_GATE_COOLDOWN = "gate_cooldown"
+
 DEFAULT_API_URL = "https://api-alpr.pirogovx.ru"
 DEFAULT_PLATE_TYPE = "auto"
+DEFAULT_ACCESS_ENABLED = False
+DEFAULT_ALLOWED_PLATES = ""
+DEFAULT_MIN_CONFIDENCE = 0.85
+DEFAULT_GATE_COOLDOWN = 30
 PLATE_TYPES = ["auto", "single_line", "two_line"]
 
 SERVICE_RECOGNIZE = "recognize"
 EVENT_PLATE_DETECTED = "alpr_ru_plate_detected"
+EVENT_ACCESS_GRANTED = "alpr_ru_access_granted"
 
 SIGNAL_RESULT = f"{DOMAIN}_result_{{entry_id}}"
 
